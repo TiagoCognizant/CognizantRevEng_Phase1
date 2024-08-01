e904896fa2f5ef9777f97f602e38cb8231713b30
Tiago Zheng <tiago.zheng23@gmail.com>
2024-08-01 11:31:08 +0100

    SMS Reciever displaying message and getting senders phone number

--- src/main/java/com/example/cognizantreveng/SmsReceiver.kt
+++ src/main/java/com/example/cognizantreveng/SmsReceiver.kt
@@ -3,13 +3,30 @@ package com.example.cognizantreveng
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.telephony.SmsMessage
 import android.util.Log
 
 class SmsReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         Log.i(TAG, "Tiago u've sms -- cognizant ")
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            val pdus = bundle["pdus"] as Array<Any>?
+            val messages: Array<SmsMessage?> = arrayOfNulls<SmsMessage>(pdus!!.size)
+            for (i in pdus!!.indices) {
+                messages[i] = SmsMessage.createFromPdu(pdus!![i] as ByteArray)
+            }
+            if (messages.size > -1) {
+                Log.i(TAG, "Message recieved: " + messages[0]?.getMessageBody())
+                Log.i(TAG, "phno is : " + messages[0]?.displayOriginatingAddress)
+
+            }
+        }
     }
+
+
 
     companion object {
         var TAG = SmsReceiver::class.java.simpleName
