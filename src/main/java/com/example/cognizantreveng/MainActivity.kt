4bd81f3f4a3b8a1e714255dd7194687981988411
Tiago Zheng Cognizant <zheng.dev23@gmail.com>
2024-08-08 10:12:51 +0100

    Start-Stop Service

--- src/main/java/com/example/cognizantreveng/MainActivity.kt
+++ src/main/java/com/example/cognizantreveng/MainActivity.kt
@@ -11,11 +11,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cognizantreveng.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
     }
 
---
+++ src/main/java/com/example/cognizantreveng/MyService.kt
@@ -0,0 +1,31 @@
+package com.example.cognizantreveng
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class MyService : Service() {
+    var TAG = MyService::class.java.simpleName
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.i(TAG, "My service created")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        Log.i(TAG,"My service started")
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "My service destroyed")
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
